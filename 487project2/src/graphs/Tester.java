c83259e3706b5333408e5a06864f8af5c015150e
Alyssa Buzzard <alyssabu@buffalo.edu>
2013-04-17 14:49:27 -0400

    Updates to testing to account for Long.parse

--- 487project2/src/graphs/Tester.java
+++ 487project2/src/graphs/Tester.java
@@ -2,21 +2,24 @@ package graphs;
 
 import java.util.List;
 
+import org.apache.hadoop.io.LongWritable;
+
 
 public class Tester {
 	
 	public static void main(String args[]){
 	//Test String Array node representation -> Node object creation
 	GraphsMapper m = new GraphsMapper();
-	String[] s = {"1","1000000","2:3","Y"};
-	Node node = m.getNode(s);
+//	String[] s = {"1","1000000","2:3","Y"};
+//	Node node = m.getNode(s);
 //	System.out.println(node);
 	
-	String testString = "1 0 2:";
-	String[] result = m.myParser(testString);
-	for(int i=0; i<result.length;i++){
-		System.out.println(result[i]);
+	String testString = "32 125 23:24:25:31:40:41:46:";
+	String[] represents = m.myParser(testString);
+	for(int i=0; i<represents.length;i++){
+		System.out.println(represents[i]);
 	}
+	LongWritable distance = new LongWritable(Long.parseLong(represents[1]));
 
 	
 	}
